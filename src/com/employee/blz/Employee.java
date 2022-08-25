1e37db2864891f65ad88f06697cc65a4d8354ce5
vikashc332 <vikashc332@gmail.com>
2022-08-25 21:12:52 +0530

    Added DailywagePartTime

--- src/com/employee/blz/Employee.java
+++ src/com/employee/blz/Employee.java
@@ -3,13 +3,15 @@ import java.lang.Math;
 
 public class Employee {
    public static void main(String args[]){
+       int full_day_hr = 8;
+       int part_time_hr = 8;
        int dailywage = 20;
-
        int present = 1;
        int  empcheck = (int) Math.floor((Math.random()*10)%2);
        if(empcheck == present){
            System.out.println("Employee is present");
-           int wage = dailywage*8;
+           int wage = dailywage * full_day_hr + dailywage * part_time_hr;
+           System.out.println("daily wage of employee " + dailywage);
            System.out.println(wage);
 
        } else{
