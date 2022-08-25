08529b2eab09aaef1485c176b00c00e59fbfe973
vikashc332 <vikashc332@gmail.com>
2022-08-25 20:00:21 +0530

    Added Dailywage

--- src/com/employee/blz/Employee.java
+++ src/com/employee/blz/Employee.java
@@ -3,10 +3,14 @@ import java.lang.Math;
 
 public class Employee {
    public static void main(String args[]){
+       int dailywage = 20;
+
        int present = 1;
        int  empcheck = (int) Math.floor((Math.random()*10)%2);
        if(empcheck == present){
            System.out.println("Employee is present");
+           int wage = dailywage*8;
+           System.out.println(wage);
 
        } else{
            System.out.println("Employee is Absent");
