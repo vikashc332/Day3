bc2f0b77c611e488c81534e4c5cf7315694ab96b
vikashc332 <vikashc332@gmail.com>
2022-09-01 21:35:08 +0530

    Added Employee Rate per hour

--- src/com/employee/blz/Employee.java
+++ src/com/employee/blz/Employee.java
@@ -2,22 +2,32 @@ package com.employee.blz;
 import java.lang.Math;
 
 public class Employee {
-   public static void main(String args[]){
-       int full_day_hr = 8;
-       int part_time_hr = 8;
-       int dailywage = 20;
-       int present = 1;
-       int  empcheck = (int) Math.floor((Math.random()*10)%2);
-       if(empcheck == present){
-           System.out.println("Employee is present");
-           int wage = dailywage * full_day_hr + dailywage * part_time_hr;
-           System.out.println("daily wage of employee " + dailywage);
-           System.out.println(wage);
+    public static final int Is_Part_time = 1;
+    public static final int Is_Full_time = 2;
+    public static final int Employ_Rate_Per_hr = 20 ;
 
-       } else{
-           System.out.println("Employee is Absent");
-       }
-   }
+    public static void main(String[] args) {
 
+        int employ_Hr = 0;
+        int employ_wage = 0;
+//Computation
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case Is_Part_time:
+                employ_Hr = 4;
+                break;
+            case Is_Full_time:
+                employ_Hr = 8;
+                break;
+            default:
+                employ_Hr = 0;
+        }
+        employ_wage = Employ_Rate_Per_hr * employ_Hr;
+        System.out.println("daily wage of employ is "+ employ_wage);
+
+    }
 
 }
+
+
+
